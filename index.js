e6bfd9580cf7be16e4db197103c516c8c8d694f9
SANGEETH RAJ <146965484+Sangeeth2402@users.noreply.github.com>
2024-11-23 12:11:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,345 @@
+// alert('warnming,,,');
+
+
+// console.log("welcome");
+
+
+// var UserNamer ="suryraj";
+// var UserAge=21;
+// var UserCity="Erode";
+
+
+/* let a =99;
+
+a=98;
+
+console.log(a); */
+
+// const a =99;
+
+// let a=98;
+
+// console.log(typeof a);
+
+
+// alert('warning...')
+//  console.log(prompt('enter Name'));
+ 
+// confirm('r u exit ?') 
+// print('')
+// close()
+// open('https://getbootstrap.com/')
+
+/* let a =10;
+
+a =a.toString()
+
+console.log(typeof a); */
+
+// parseFloat(prompt("enter your data"))
+
+/* let result = parseFloat("10.00");
+console.log(result);  */
+
+
+
+let UserName =  "   Suryraj  ";
+
+// console.log(UserName[5]);
+// console.log(UserName.length);
+// console.log(UserName.concat(" welcome"));
+// console.log(UserName.indexOf('r'));
+// console.log(UserName.lastIndexOf('r'));
+// console.log(UserName.repeat(5));
+// console.log(UserName.toUpperCase());
+// console.log(UserName.trim());
+
+// UserName[18]=" sangeeth";
+
+// console.log(UserName);
+
+// console.log(UserName + "Hello ");
+
+// console.log(`welcome   ${UserName}     asdfghjkl;`);  
+
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(90));
+// console.log(Math.ceil(9.1));
+// console.log(Math.min(90,80));
+
+// console.log(Math.max(90,79));
+// console.log(Math.pow(2,5));
+// console.log(Math.random());
+// console.log(Math.sqrt(64));
+// console.log(Math.random());
+// console.log(Math.floor(90.9));
+
+
+
+// Arthimetic    + - / % *;
+
+// Assinmnrt   = !=  =<   >=
+
+// Compare   < >  ==  === 
+
+// AND  &&  OR ||    NOT !
+
+// ++  -- 
+
+//  XOR ^
+
+// value ?  true  : false
+
+
+
+// let ar = [1,2,123.3,'s',"surya"];
+
+// console.log(ar);
+// console.log(ar.length);
+// console.log(ar[0]);
+
+// ar[6]="welcome";
+
+// console.log(ar[5]);
+
+// ar.push(90)
+// ar.push(91)
+// ar.push(92)
+
+// ar.pop();
+
+// ar.unshift(89)
+// ar.unshift(90)
+// ar.unshift(91)
+
+// ar.shift();
+
+// ar.splice(3,0,"KIA","BMW")
+
+
+// console.log(ar.slice(3,9));
+
+// console.log(ar);
+
+
+
+
+// let arr = [
+//     [1,2,3,4,5,6],
+//     ['s','u','r','y','a']
+// ];
+
+// console.log(arr[1][0])
+
+// const ar = [12,32,34,5];
+
+// ar.push('surya');
+
+// console.log(ar);
+
+// if(10<20){
+//     console.log("true")
+// }
+// else if(90<50){
+//     console.log("wrong2")
+
+// }
+// else{
+//     console.log("wronf")
+    
+// }
+
+// if(90<98){
+//     if(90<80){
+//         if(80<89){
+//     console.log("true")
+
+//         }
+//     }
+//     else{
+//     console.log("wrong")
+
+//     }
+// } 
+
+
+// SWITCH CASE:
+// let a =prompt("enter a number");
+//     switch(a){
+//         case 0:
+//         console.log("One");
+//         break;
+
+//         case 1:
+//         console.log("Two");
+//         break;
+
+
+//         default :
+//         console.log("error");
+//         break;
+//     }
+
+
+// let ar = [1,23,333.44,'s',"suryraj"]
+
+// console.log(ar);
+// console.log(ar[0]);
+// console.log(ar.length);
+// console.log(ar.concat(ar));
+
+// ar[6]="sangeet";
+
+
+// ar.push('s1')
+// ar.push('s2')
+// ar.push('s3')
+
+
+// ar.pop()
+
+
+// ar.unshift(90)
+// ar.unshift(91)
+// ar.unshift(93)
+
+// ar.shift()
+
+
+// ar.splice(3,1,"kia","BMW");
+
+
+
+// console.log(ar);
+
+
+
+
+
+
+// let ar =[
+//     ['s','u','r','a'],
+//     [1,2,3,4,5,['i','d','m']]
+// ]
+
+
+// console.log(ar[1][5][1]);
+
+// const arr = [1,2,34];
+
+// arr.push('surya');
+
+// console.log(arr);
+
+
+// let obj ={
+//     Name:"Suryraj",
+//     Age:21,
+//     city:"erode",
+//     brother:{
+//         Name:"sanggeth"
+//     }
+    
+
+// }
+
+// console.log(obj);
+// console.log(obj.Age);
+// console.log(obj.brother.Name);
+
+
+// let ArrObj =[
+//     {Name:"Suryraj1"},
+//     {Name:"Suryraj2"},
+//     {Name:"Suryraj3"},
+//     {Name:"Suryraj4"},
+//     {Name:"Suryraj6"},
+//     {Name:"Suryraj7"},
+//     {Name:"Suryraj8"},
+//     {Name:"Suryraj9"},
+//     {Name:"Suryraj5",Brother:{
+//         Name:"Sanggetthh"
+//     }},
+    
+// ]
+
+
+// for(let i=0;i<10;i++){   // i=i+1
+
+//         console.log(i);
+        
+// }
+
+// let a = 1
+// while(a<20){
+//     console.log(a);
+//         a++;
+// };
+
+// do{
+//     console.log(a);
+//     a++;
+//     if(a==15){
+//         break;
+//     }
+  
+    
+// }while(a<20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
